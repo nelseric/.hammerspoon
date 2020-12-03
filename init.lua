f1e666fa2cfc14b2d508a29b79654d4b8f9367b1
Eric Nelson <nelseric.alt@gmail.com>
2020-12-03 15:09:57 -0600

    remove unnecesary hotky now that I'm using yabai

--- init.lua
+++ init.lua
@@ -1,51 +1,5 @@
 hs.application.enableSpotlightForNameSearches(true)
 
-hs.hotkey.bind({'cmd'}, 'escape', function()
-  local laptopScreen = hs.screen.primaryScreen()
-  local secondScreen = hs.screen{x=1,y=0} or laptopScreen
-  local thirdScreen  = hs.screen{x=2,y=0} or secondScreen
-  local fourthScreen = hs.screen{x=2,y=0} or thirdScreen
-
-  local frame = thirdScreen:frame();
-
-  local windowLayout = {
-    {
-      'Spotify', nil, thirdScreen, nil,
-      thirdScreen:localToAbsolute(hs.geometry{
-        0,
-        0,
-        frame.w,
-        frame.h * 0.5
-      }), nil
-    },
-    {
-      'Slack', nil, thirdScreen, nil,
-      thirdScreen:localToAbsolute(hs.geometry{
-        0, frame.h/2,
-        frame.w, frame.h/2
-      }), nil
-    },
-    -- {
-    --   'iTerm', 'Dev', thirdScreen, nil,
-    --   hs.geometry{
-    --     frame.x, frame.y, 962.0, 592.0
-    --   }
-    -- },
-    -- {
-    --   'OmniFocus', nil, thirdScreen, nil,
-    --   hs.geometry{
-    --     frame.x, frame.y+frame.h/2,
-    --     frame.w/2, frame.h/2
-    --   }
-    -- }
-  }
-  hs.layout.apply(windowLayout, string.match)
-
-  for k,v in pairs(windowLayout) do
-    hs.application.launchOrFocus(v[1])
-  end
-end)
-
 hs.hotkey.bind({'ctrl'}, 'escape', function()
   local win = hs.window.focusedWindow()
   win:setFrame(win:screen():frame(  ))
